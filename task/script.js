db0376f2b3d138c55815d5948121ed0ccdebb50e
Alexey.Tsitrkop <alexey.tsitrkop@7bits.it>
2015-07-27 12:37:04 +0600

    change task of js

--- task/script.js
+++ task/script.js
@@ -1,11 +1,21 @@
 'use strict';
 var gulp = require('gulp');
+var watch = require('gulp-watch');
+var path = require('../conf');
 
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 
-gulp.task('js', function() {
+gulp.task('js:dev', function() {
   return gulp.src(path.js.src)
+    .pipe(watch(path.js.src))
+    .pipe(gulp.dest(path.js.build));
+});
+
+gulp.task('js:prod', function() {
+  return gulp.src(path.js.src)
+    .pipe(concat('script.js'))
     .pipe(uglify())
     .pipe(gulp.dest(path.js.build));
 });
